0efae357eece94f5daa09ff2f21719a7ca8ac0b5
Jose A. Velasco <jvelasc3@uci.edu>
2022-09-13 16:32:42 -0700

    Improved computer and player choice functions

--- script.js
+++ script.js
@@ -1,40 +1,22 @@
-function rock() {
-  return "I pick Rock";
-}
-
-function paper() {
-  return "I pick paper";
-}
-
-function scissor() {
-  return "I pick scissor";
-}
-
-function getHumanChoice() {
-  let choice = prompt("Pick Rock, Paper or Scissor");
-  let answer = choice.toLowerCase();
-
-  if (answer === "rock") {
-    return rock();
-  } else if (answer === "paper") {
-    return paper();
-  } else if (answer === "scissor") {
-    return scissor();
-  } else {
-    return "error";
-  }
-}
+const options = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
-  let choice = ["rock", "paper", "scissor"];
-  let answer = choice[Math.floor(Math.random() * choice.length)];
-  if (answer === "rock") {
-    return rock();
-  } else if (answer === "paper") {
-    return paper();
-  } else if (answer === "scissor") {
-    return scissor();
-  } else {
-    return "error";
+  let choice = options[Math.floor(Math.random() * options.length)];
+  console.log(choice);
+  return choice;
+}
+
+function getPlayerChoice() {
+  let errorInput = false;
+  while (errorInput === false) {
+    const choice = prompt("Pick: Rock, Paper or Scissors?");
+    if (choice == null) {
+      continue;
+    }
+    const choiceLower = choice.toLowerCase();
+    if (options.includes(choiceLower)) {
+      validatedInput = true;
+      return choiceLower;
+    }
   }
 }
